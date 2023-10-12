7b0b34416a2cb451d1ffc49cdb5a87b12caddf10
erwynmurphy711 <erwynmurphy711@gmail.com>
2023-10-12 13:23:53 +0800

    yay

--- src/main/java/com/example/Square.java
+++ src/main/java/com/example/Square.java
@@ -1,18 +1,22 @@
 package com.example;
 
 public class Square extends Shape {
-
+    private int side;
+   
+    public Square(int side){
+        this.side = side;
+    }
     // TODO: Define attributes for the square
 
     @Override
     public double area() {
         // TODO: Implement the method
-        return 0;
+        return this.side*this.side;
     }
 
     @Override
     public double perimeter() {
         // TODO: Implement the method
-        return 0;
+        return this.side*4;
     }
 }
--- src/main/java/com/example/Triangle.java
+++ src/main/java/com/example/Triangle.java
@@ -1,19 +1,26 @@
 package com.example;
 
 public class Triangle extends Shape {
-
+    private int side1;
+    private int side2;
+    private int side3;
     // TODO: Define attributes for the triangle
-
+    public Triangle(int side1, int side2, int side3){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
     @Override
     public double area() {
+        double s = (this.side1+this.side2+this.side3)/2;
         // TODO: Implement the method
-        return 0;
+        return Math.sqrt(s*(s-this.side1)*(s-this.side2)*(s-this.side3));
     }
 
     @Override
     public double perimeter() {
         // TODO: Implement the method
-        return 0;
+        return this.side1+this.side2+this.side3;
     }
 }
 
